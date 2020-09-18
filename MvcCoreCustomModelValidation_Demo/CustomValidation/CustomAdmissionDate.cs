91773d2b65a80e0fffe770b0daf6c443f68e0562
Harshita <harshitavaghani11@gmail.com>
2020-09-18 10:13:37 +0530

    Add project files.

---
+++ MvcCoreCustomModelValidation_Demo/CustomValidation/CustomAdmissionDate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcCoreCustomModelValidation_Demo.CustomValidation
+{
+    public class CustomAdmissionDate : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            DateTime dateTime = Convert.ToDateTime(value);
+            return dateTime <= DateTime.Now;
+        }
+    }
+}
